f0b40c1f479fc2403b756bcba1bd7682caf8a001
gitName <a@b.c>
2021-12-06 14:26:00 +0700

    first commit

---
+++ src/test/java/com/example/springazuredemomain/SpringAzureDemoMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springazuredemomain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAzureDemoMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
